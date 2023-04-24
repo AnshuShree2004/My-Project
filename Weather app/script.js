b430ef3cf48ef95ff080593f6c858c1f9040d296
AnshuShree2004 <ianshushree@gmail.com>
2023-04-24 07:12:41 +0530

    first commit

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,7 +1,7 @@
-const options = {
+/*const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Key': '2eee8efde7msh5d2dea03fc174fep18d485jsn64a1c1cd747b',
 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 }
@@ -13,6 +13,7 @@ cityname.innerHTML= city;
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
 	.then(response => response.json())
 	.then((response) => {
+
 		console.log(response)
 	cloud_pct.innerHTML = response.cloud_pct
     temp.innerHTML = response.temp
@@ -26,7 +27,7 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, op
 	.catch(err => console.error(err));
 }
 
-submit.addEventListener('click',(e) =>{
+search.addEventListener('click',(e) =>{
 	e.preventDefault()
 	getWeather(city.value)
  
@@ -35,4 +36,44 @@ submit.addEventListener('click',(e) =>{
 
 getWeather('Delhi')
 
+*/
+
+const cityname = document.getElementById('cityname');
+const cloud_pct = document.getElementById('cloud_pct');
+const temp = document.getElementById('temp');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind_speed');
+//const search = document.getElementsById('search');
+const city = document.getElementById('city');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2eee8efde7msh5d2dea03fc174fep18d485jsn64a1c1cd747b',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+}
+
+const getWeather = (city) => {
+  cityname.textContent = city;
+
+  fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options)
+    .then(response => response.json())
+    .then((response) => {
+      console.log(response);
+      cloud_pct.textContent= response.cloud_pct;
+      temp.textContent = response.temp;
+      humidity.textContent = response.humidity;
+      wind_speed.textContent = response.wind_speed;
+    })
+    .catch(err => console.error(err));
+};
+
+search.addEventListener('click', (e) => {
+	
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather('Delhi');
 
